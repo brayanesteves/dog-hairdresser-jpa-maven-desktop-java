1e4ed21cb9c9057e4f08124b632b423e6d47da0e
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-07 17:57:44 -0400

    Structure Base [07/10/2023]

---
+++ src/main/java/com/halconbit/dog/hairdresser/jpa/java/DogHairdresserJpaJava.java
@@ -0,0 +1,12 @@
+package com.halconbit.dog.hairdresser.jpa.java;
+
+/**
+ *
+ * @author Brayan Esteves
+ */
+public class DogHairdresserJpaJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
