fcc2379a9167f8e3ecd3751bf8adfa1837c2248e
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-09 10:17:50 -0400

    Statement - Dog Hairdresser (JPA) [09/08/2023]

--- src/main/java/com/halconbit/dog/hairdresser/jpa/java/DogHairdresserJpaJava.java
+++ src/main/java/com/halconbit/dog/hairdresser/jpa/java/DogHairdresserJpaJava.java
@@ -25,6 +25,13 @@ package com.halconbit.dog.hairdresser.jpa.java;
  * 8) Owner's Phone Number (Label, Input)
  * 9) Observations (Label, Textarea)
  * 10) 2 Buttons (Actions: Clear and Save)
+ * Since the <b>data</b> needs to <i>persist over time</i> and for the future, the employees of
+ * the grooming salon will be able to access them, it is required that each of the
+ * records be stored in a <b>database<*b>.
+ * Based on this requirements gathering:
+ * 1) Develop a <b>desktop application</b> that is able to register, in a database,
+ *    the data, both pets and owners that are entered from a graphical user interface.
+ * 2) For this development, take into account the concepts of the <b>LAYERED MODEL.</b>
  * [=======]
  * [SPANISH]:
  * Ejercicio Integrador: Peluqueria Canina.
@@ -49,6 +56,14 @@ package com.halconbit.dog.hairdresser.jpa.java;
  * 8) Celular Dueño (Label, Input)
  * 9) Observaciones (Label, Textarea)
  * 10) 2 Botones (Acciones: Limpiar y Guardar)
+ * Cómo los <b>datos</b> deben <i>permanecer en  el tiempo</i> y a futuro, los
+ * empleados de la peluqueria van a poder a acceder a ellos, se requiere que cada
+ * uno de los registros sean almacenados en una <b>base de datos.</b>
+ * A partir de este relevamiento de requerimientos:
+ * 1) Realizar el desarrollo de una <b>aplicación de escritorio0</b>, que sea capaz de
+ *    registrar, en una base de datos, los datos, tanto de mascotas, cómo dueños
+ *    que se ingresen desde una interfaz gráfica de usuario.
+ * 2) Para este desarrollo, tener en cuenta los conceptos del <b>MODELO DE CAPAS.</b>
  */
 public class DogHairdresserJpaJava {
 
