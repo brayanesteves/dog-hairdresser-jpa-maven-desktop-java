46ce5c6c36ffa03801c8442e7245c9099264b13d
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-09 14:15:16 -0400

    GUI - Add Data (Dog Hairdresser) [09/10/2023]

--- src/main/java/com/halconbit/dog/hairdresser/jpa/java/DogHairdresserJpaJava.java
+++ src/main/java/com/halconbit/dog/hairdresser/jpa/java/DogHairdresserJpaJava.java
@@ -3,14 +3,15 @@ package com.halconbit.dog.hairdresser.jpa.java;
 /**
  * @author Brayan Esteves
  * Challenge:
- * [ENGLISH]:
- * Integrating Exercise: Dog Grooming Salon. A dog grooming salon needs <b>a form</b>
- * to <b>store the data of each of its client pets.</b> For this, it requests the
+ * <b>[ENGLISH]:</b>
+ * Integrating Exercise: Dog Grooming Salon.
+ * A dog grooming salon needs <b>a form</b> to <b>store the data of each of its
+ * client pets.</b> For this, it requests the
  * development of an application that is able to register the following data for
  * each of them and their owners:
- * 1) Pets: num_clientes, numbre_perro, raza, color, alergico, atencion_especial,
- * observaciones.
- * 2) Owner: id_dueño, nombre, celular, direccion.
+ * <b>1) Pets:</b> customer_number, dog_name, breed, color, allergic, special_care,
+ *                 observations.
+ * <b>2) Owner:</b> owner_id, name, cellphone_number, address.
  * In order to easily register and be attractive to the user, the owner of the
  * dog grooming salon provides the following <b>APPROXIMATE</b> design of the idea
  * she has for the user interface:
@@ -33,14 +34,14 @@ package com.halconbit.dog.hairdresser.jpa.java;
  *    the data, both pets and owners that are entered from a graphical user interface.
  * 2) For this development, take into account the concepts of the <b>LAYERED MODEL.</b>
  * [=======]
- * [SPANISH]:
+ * <b>[SPANISH]:</b>
  * Ejercicio Integrador: Peluqueria Canina.
  * Una peluqueria canina necesita de <b>un formulario</b> para <b>almacenar los datos
  * de cada una de sus mascotas clientes.</b> Para ello, solicita el desarrollo de
  * una aplicación que sea capaz de registrar los siguientes de datos de cada una
  * de ellas y sus dueños:
  * <b>1) Mascotas:</b> num_clientes, numbre_perro, raza, color, alergico, atencion_especial,
- * observaciones.
+ *                     observaciones.
  * <b>2) Dueño:</b> id_dueño, nombre, celular, direccion.
  * Para poder registrar de manera sencilla y que sea atractiva para el usuario,
  * la dueña de la peluqueria canina proporciona el siguiente diseño <b>APROXIMADO</b>
