3d559e094f17dac916e2bbf2c45e6f51499371ab
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-08 21:44:59 -0400

    Declaration [08/10/2023]

--- src/main/java/com/halconbit/dog/hairdresser/jpa/java/DogHairdresserJpaJava.java
+++ src/main/java/com/halconbit/dog/hairdresser/jpa/java/DogHairdresserJpaJava.java
@@ -1,8 +1,54 @@
 package com.halconbit.dog.hairdresser.jpa.java;
 
 /**
- *
  * @author Brayan Esteves
+ * Challenge:
+ * [ENGLISH]:
+ * Integrating Exercise: Dog Grooming Salon. A dog grooming salon needs <b>a form</b>
+ * to <b>store the data of each of its client pets.</b> For this, it requests the
+ * development of an application that is able to register the following data for
+ * each of them and their owners:
+ * 1) Pets: num_clientes, numbre_perro, raza, color, alergico, atencion_especial,
+ * observaciones.
+ * 2) Owner: id_dueño, nombre, celular, direccion.
+ * In order to easily register and be attractive to the user, the owner of the
+ * dog grooming salon provides the following <b>APPROXIMATE</b> design of the idea
+ * she has for the user interface:
+ * 0) Title: Dog Grooming Salon.
+ * 1) Client Number (Label, Input)
+ * 2) Name (Label, Input)
+ * 3) Breed (Label, Input)
+ * 4) Color (Label, Input)
+ * 5) Allergic (Label, Select)
+ * 6) Special Attention (Label, Select)
+ * 7) Owner's Name (Label, Input)
+ * 8) Owner's Phone Number (Label, Input)
+ * 9) Observations (Label, Textarea)
+ * 10) 2 Buttons (Actions: Clear and Save)
+ * [=======]
+ * [SPANISH]:
+ * Ejercicio Integrador: Peluqueria Canina.
+ * Una peluqueria canina necesita de <b>un formulario</b> para <b>almacenar los datos
+ * de cada una de sus mascotas clientes.</b> Para ello, solicita el desarrollo de
+ * una aplicación que sea capaz de registrar los siguientes de datos de cada una
+ * de ellas y sus dueños:
+ * <b>1) Mascotas:</b> num_clientes, numbre_perro, raza, color, alergico, atencion_especial,
+ * observaciones.
+ * <b>2) Dueño:</b> id_dueño, nombre, celular, direccion.
+ * Para poder registrar de manera sencilla y que sea atractiva para el usuario,
+ * la dueña de la peluqueria canina proporciona el siguiente diseño <b>APROXIMADO</b>
+ * de la idea que tiene para la interfaz gráfica de usuario:
+ * 0) Titulo: Peluqueria Canina.
+ * 1) Cliente Nº (Label, Input)
+ * 2) Nombre (Label, Input)
+ * 3) Raza (Label, Input)
+ * 4) Color (Label, Input)
+ * 5) Alergico (Label, Select)
+ * 6) Atención Especial (Label, Select)
+ * 7) Nombre Dueño (Label, Input)
+ * 8) Celular Dueño (Label, Input)
+ * 9) Observaciones (Label, Textarea)
+ * 10) 2 Botones (Acciones: Limpiar y Guardar)
  */
 public class DogHairdresserJpaJava {
 
