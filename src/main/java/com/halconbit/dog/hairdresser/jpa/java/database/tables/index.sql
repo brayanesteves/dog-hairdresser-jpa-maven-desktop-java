d3e23aba214c9c81cc98dfc83d1715cf3aa2ee21
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-12 15:07:09 -0400

    Refactor Tables [12/11/2023]

--- src/main/java/com/halconbit/dog/hairdresser/jpa/java/database/tables/index.sql
+++ src/main/java/com/halconbit/dog/hairdresser/jpa/java/database/tables/index.sql
@@ -1,19 +1,20 @@
 CREATE TABLE IF NOT EXISTS `dog_hairdresser`.`owner` (
-    `owner_id`         INT(255)     NOT NULL AUTO_INCREMENT COMMENT '',
+    `owner_id`         INT    (255) NOT NULL AUTO_INCREMENT COMMENT '',
     `name`             VARCHAR(50)  NOT NULL                COMMENT '',
-    `cellphone_number` VARCHAR(20) NOT NULL                 COMMENT '',
+    `cellphone_number` VARCHAR(20)  NOT NULL                 COMMENT '',
     `address`          TEXT NULL
     PRIMARY KEY (`owner_id`)
 ) ENGINE='InnoDB' DEFAULT CHARSET='utf8' COLLATE='utf8_bin' COMMENT='';
 
 
 CREATE TABLE IF NOT EXISTS `dog_hairdresser`.`pet` (
-    `customer_number` INT(255)     NOT NULL AUTO_INCREMENT COMMENT '',
+    `customer_number` INT    (255) NOT NULL AUTO_INCREMENT COMMENT '',
     `dog_name`        VARCHAR(20)  NOT NULL                COMMENT '',
     `breed`           VARCHAR(255) NOT NULL                COMMENT '',
     `color`           VARCHAR(20)      NULL                COMMENT '',
     `allergic`        VARCHAR(20)      NULL                COMMENT '',
     `special_care`    VARCHAR(20)      NULL                COMMENT '',
     `observations`    TEXT             NULL                COMMENT '',
+    `owner_id_owner`  INT    (255) NOT NULL                COMMENT '',
     PRIMARY KEY (`customer_number`)
 ) ENGINE='InnoDB' DEFAULT CHARSET='utf8' COLLATE='utf8_bin' COMMENT='';
